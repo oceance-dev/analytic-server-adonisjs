8cac8c375d990d1c111d87ca5f77d25c1ea7bbfe
oceance-dev <oceance.dev@gmail.com>
2026-04-14 12:31:00 +0200

    Delete app directory

--- app/validators/user.js
+++
@@ -1,7 +0,0 @@
-import vine from '@vinejs/vine';
-export const createPostUser = vine.compile(vine.object({
-    fullName: vine.string().trim().minLength(4).maxLength(100),
-    email: vine.string().email(),
-    password: vine.string()
-}));
-//# sourceMappingURL=user.js.map
